598ba08696217fde93851dbec0f598b2219c1c79
Feer Batista ( Nasa ) <134456146+feerbatista@users.noreply.github.com>
2023-06-06 02:07:57 +0000

    alterando página inicial

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home(){
         <div><h1>Welcome!</h1>
         <p>Essa pessoa especial é você !</p>
         <p>Me diga qual foi sua reação nos comentários</p>
-        <a href="https://github.com/feerbatista">Me siga no Github</a>
+        <a href="https://github.com/feerbatista">Me siga no Github</a><br></br>
         <a href="https://www.instagram.com/feerbatista98/">Meu instragram</a>
 
         </div>
