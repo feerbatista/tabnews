6d99db99ea60adca6bb07712a4f61a46a78a72e6
Feer Batista ( Nasa ) <134456146+feerbatista@users.noreply.github.com>
2023-06-06 02:06:37 +0000

    adicionando instagram

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home(){
         <p>Essa pessoa especial é você !</p>
         <p>Me diga qual foi sua reação nos comentários</p>
         <a href="https://github.com/feerbatista">Me siga no Github</a>
+        <a href="https://www.instagram.com/feerbatista98/">Meu instragram</a>
 
         </div>
     )
