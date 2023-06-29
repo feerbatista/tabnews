905a9ca24c17f5195df7c13e4d678612f8646fd2
Feer Batista ( Nasa ) <134456146+feerbatista@users.noreply.github.com>
2023-06-29 23:26:59 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-function Home(){
-    return(
-        <div><h1>Welcome!</h1>
-        <p>Essa pessoa especial é você !</p>
-        <p>Me diga qual foi sua reação nos comentários</p>
-        <a href="https://github.com/feerbatista">Me siga no Github</a><br></br>
-        <a href="https://www.instagram.com/feerbatista98/">Meu instragram</a>
-
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Essa pessoa especial é você !</p>
+      <p>Me diga qual foi sua reação nos comentários</p>
+      <a href="https://github.com/feerbatista">Me siga no Github</a>
+      <br></br>
+      <a href="https://www.instagram.com/feerbatista98/">Meu instragram</a>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
